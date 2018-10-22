d87d4b6fc9d6fe84812639c2b0a1456f43eaa136
Elliott Smith <elliottsmith1@hotmail.co.uk>
2018-10-22 17:10:49 +0100

    initial

---
+++ Data Visualisation/Assets/Assets/Scripts/SliderText.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderText : MonoBehaviour {
+
+    [SerializeField] Slider slider_ref;
+    private Text text_ref;
+
+	// Use this for initialization
+	void Start ()
+    {
+        text_ref = this.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		switch ((int)slider_ref.value)
+        {
+            case 0:
+                text_ref.text = "2007";
+                break;
+            case 1:
+                text_ref.text = "2008";
+                break;
+            case 2:
+                text_ref.text = "2009";
+                break;
+            case 3:
+                text_ref.text = "2010";
+                break;
+            case 4:
+                text_ref.text = "2011";
+                break;
+            case 5:
+                text_ref.text = "2012";
+                break;
+            case 6:
+                text_ref.text = "2013";
+                break;
+            case 7:
+                text_ref.text = "2014";
+                break;
+        }
+	}
+}
---
+++ Data Visualisation/Assets/Scripts/DiseaseStats.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiseaseStats : MonoBehaviour {
+
+    public string death_cause;
+    public string race;
+    public string sex;
+    public int year;
+    public float deaths;
+    public float death_rate;
+    public float age_adjusted_death_rate;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
