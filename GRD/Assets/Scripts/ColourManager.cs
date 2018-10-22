daa5e1484f81173833d95e1b7c7f91d9654670f1
Matthew <Matthew2.Churchyard@live.uwe.ac.uk>
2018-10-22 21:36:59 +0100

    Colour Scene added

---
+++ GRD/Assets/Scripts/ColourManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColourManager : MonoBehaviour {
+
+	[SerializeField] Slider slider_col;
+	public int colourDivider = 1;
+	public int maxDeaths = 100;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		switch ((int)slider_col.value)
+		{
+			case 0:
+				colourDivider = 1;
+				maxDeaths = 100;
+				break;
+			case 1:
+				colourDivider = 2;
+				maxDeaths = 200;
+				break;
+			case 2:
+				colourDivider = 3;
+				maxDeaths = 300;
+				break;
+			case 3:
+				colourDivider = 4;
+				maxDeaths = 400;
+				break;
+			case 4:
+				colourDivider = 5;
+				maxDeaths = 500;
+				break;
+		}
+	}
+}
---
+++ GRD/Assets/Scripts/OrbColourDefiner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbColourDefiner : MonoBehaviour {
+
+	public int deathCount;
+	public int ethnicities;
+	ColourManager CM;
+	Renderer OrbRend;
+	Vector4 colourToApply;
+	float deathCalculation;
+	public Material OrbMat;
+
+	// Use this for initialization
+	void Start () {
+		CM = FindObjectOfType<ColourManager>();
+		OrbRend = GetComponent<Renderer>();
+		deathCalculation = (CM.maxDeaths/ deathCount);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (deathCalculation > CM.colourDivider) {
+			OrbRend.material.color = new Color(1f, 1f, 1f, 0f);
+			Color color = OrbMat.color;
+			color.a = 0;
+			OrbMat.color = color;
+		}
+		
+	}
+}
