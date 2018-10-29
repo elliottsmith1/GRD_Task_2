3caad9e65d1c8e03cba37cb3a6c72a597806373a
Elliott Smith <elliott2.smith@live.uwe.ac.uk>
2018-10-29 01:51:55 +0000

    Camera movement script

--- GRD/Assets/CameraControl.cs
+++ GRD/Assets/CameraControl.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour {
 
+    [SerializeField] float speed = 5.0f;
     private Vector3 look_at = new Vector3(0, 0, 0);
 
 	// Use this for initialization
@@ -14,6 +15,16 @@ public class CameraControl : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        transform.LookAt(look_at);
-	}
+        transform.LookAt(look_at);        
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.RotateAround(Vector3.zero, -Vector3.up, speed * Time.deltaTime);
+        }
+
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.RotateAround(Vector3.zero, Vector3.up, speed * Time.deltaTime);
+        }
+    }
 }
